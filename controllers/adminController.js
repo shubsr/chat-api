15fa70074789d5d17cec29c555f1f8d5c2be3bf7
Shubham Rathi <shubhamsinghrathi@gmail.com>
2017-06-22 12:34:04 +0530

    Phase 2 is completed

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -20,69 +20,102 @@ module.exports = function(app) {
     });
   });
 
-  //   app.post("/aa-user-chat", jsonParser, function(req, res) {
+  app.post("/admin-chat", jsonParser, function(req, res) {
+    if (!req.body) {
+      return res.sendStatus(404);
+    }
+    if (!req.body.code || !req.body.time || !req.body.userCode) {
+      return res.sendStatus(404);
+    }
+    if (req.body.code != "admin") {
+      return res.sendStatus(404);
+    }
+    Models.User.findOne({ code: req.body.userCode }).exec(function(err, data) {
+      if (err) {
+        return res.sendStatus(400);
+      }
+      if (!data) {
+        return res.sendStatus(404);
+      }
+      Models.Chat
+        .find({ time: { $gt: req.body.time }, code: req.body.userCode })
+        .exec(function(er, dt) {
+          if (er) {
+            return res.sendStatus(400);
+          }
+          return res.json(dt);
+        });
+    });
+  });
 
-  //     if (!req.body) {
+  app.post("/admin-responded", jsonParser, function(req, res) {
+    if (!req.body) {
+      return res.sendStatus(404);
+    }
+    if (!req.body.code || !req.body.userCode) {
+      return res.sendStatus(404);
+    }
+    if (req.body.code != "admin") {
+      return res.sendStatus(404);
+    }
 
-  //       return res.sendStatus(400);
+    Models.User.update(
+      { code: req.body.userCode },
+      { $set: { responded: 1 } },
+      function(err, data) {
+        if (err) {
+          return res.sendStatus(404);
+        }
+        data_send = { status: 1 };
+        return res.json(data_send);
+      }
+    );
+  });
 
-  //     }
+  app.post("/admin-sender", jsonParser, function(req, res) {
+    if (!req.body) {
+      return res.sendStatus(404);
+    }
+    if (!req.body.code || !req.body.msg || !req.body.userCode) {
+      return res.sendStatus(404);
+    }
+    if (req.body.code != "admin") {
+      return res.sendStatus(404);
+    }
 
-  //     if (!req.body.code || !req.body.msg) {
+    var query = Models.User.findOne({ code: req.body.userCode });
 
-  //       return res.sendStatus(400);
+    query.exec(function(er, dt) {
+      if (er) {
+        return res.sendStatus(404);
+      }
+      if (!dt) {
+        return res.sendStatus(404);
+      }
+      if (req.body.code != "admin") {
+        return res.sendStatus(404);
+      }
 
-  //     }
+      var d = new Date();
 
-  //     var query = User.findOne({ code: req.body.code });
+      var currentTime = d.getTime();
 
-  //     query.exec(function(er, dt) {
+      var content = {
+        code: req.body.userCode,
+        msg: req.body.msg,
+        sender: 0,
+        time: currentTime
+      };
 
-  //       if (er) {
+      var saver = new Models.Chat(content);
 
-  //         return res.sendStatus(404);
-
-  //       }
-
-  //       if (!dt) {
-
-  //         return res.sendStatus(404);
-
-  //       }
-
-  //       var d = new Date();
-
-  //       var currentTime = d.getTime();
-
-  //       var content = {
-
-  //         code: req.body.code,
-
-  //         msg: req.body.msg,
-
-  //         sender: 1,
-
-  //         time: currentTime
-
-  //       };
-
-  //       var saver = new Chat(content);
-
-  //       saver.save(function(err, data) {
-
-  //         if (err) {
-
-  //           return res.sendStatus(404);
-
-  //         }
-
-  //         data_send = { status: 1 };
-
-  //         return res.json(data_send);
-
-  //       });
-
-  //     });
-
-  //   });
+      saver.save(function(err, data) {
+        if (err) {
+          return res.sendStatus(404);
+        }
+        data_send = { status: 1 };
+        return res.json(data_send);
+      });
+    });
+  });
 };
